9576bbe9ab44c47939b56ad966188e966b43c98c
Good Wood <slm_1990@126.com>
2025-05-26 18:20:53 +0800

    feat: add author_name for app list card (#16900)
    Co-authored-by: crazywoola <427733928@qq.com>
    Co-authored-by: crazywoola <100913391+crazywoola@users.noreply.github.com>

--- api/fields/app_fields.py
+++ api/fields/app_fields.py
@@ -101,6 +101,8 @@ app_partial_fields = {
     "tags": fields.List(fields.Nested(tag_fields)),
     "asa_company_id": fields.String,
     "access_mode": fields.String,
+    "create_user_name": fields.String,
+    "author_name": fields.String,
 }
 
 
